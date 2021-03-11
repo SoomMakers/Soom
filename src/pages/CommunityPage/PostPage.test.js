c3e87ca644655a3f80c2ff359f5225ccdb47e2e4
dodoongtak <themighty.tak@gmail.com>
2021-03-12 03:02:53 +0900

    create PostPage

---
+++ src/pages/CommunityPage/PostPage.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import PostPage from './PostPage';
+
+describe('CommunityPage', () => {
+    it('renders PostPage', () => {
+        
+    })
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
-export { default as CommunityPage} from './CommunityPage'
+export { default as CommunityPage} from './CommunityPage/CommunityPage'
+export { default as PostPage } from './CommunityPage/PostPage';
 export { default as TodoPage} from './TodoPage'
 export { default as TodoFindPage} from './TodoFindPage'
