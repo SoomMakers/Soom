e9e7036ded7b3bd185e3d3fec129e74aa4603ba6
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-15 21:38:16 +0900

    Feat: add PostContainer

--- src/fixtures/asia.js
+++ src/fixtures/asia.js
@@ -1,97 +1,97 @@
 
 const asia = {
-    posts: [
-      {
-        user: {
-          id: 100,
-          name: 'kim',
-        },
-        post: {
-          todo: [
-            {
-              id: 1000,
-              taskTitle: 'Segregation',
-            },
-            {
-              id: 1001,
-              taskTitle: 'Recycle Plastics',
-            },
-          ],
-        },
+  posts: [
+    {
+      user: {
+        id: 100,
+        name: 'kim',
       },
-      {
-        user: {
-          id: 101,
-          name: 'park',
-        },
-        post: {
-          todo: [
-            {
-              id: 1000,
-              taskTitle: 'Segregation',
-            },
-            {
-              id: 1001,
-              taskTitle: 'Recycle Plastics',
-            },
-          ],
-        },
+      post: {
+        todo: [
+          {
+            id: 1000,
+            taskTitle: 'Segregation',
+          },
+          {
+            id: 1001,
+            taskTitle: 'Recycle Plastics',
+          },
+        ],
       },
-      {
-        user: {
-          id: 102,
-          name: 'shin',
-        },
-        post: {
-          todo: [
-            {
-              id: 1000,
-              taskTitle: 'Segregation',
-            },
-            {
-              id: 1001,
-              taskTitle: 'Recycle Plastics',
-            },
-          ],
-        },
+    },
+    {
+      user: {
+        id: 101,
+        name: 'park',
       },
-      {
-        user: {
-          id: 103,
-          name: 'choi',
-        },
-        post: {
-          todo: [
-            {
-              id: 1000,
-              taskTitle: 'Segregation',
-            },
-            {
-              id: 1001,
-              taskTitle: 'Recycle Plastics',
-            },
-          ],
-        },
+      post: {
+        todo: [
+          {
+            id: 1000,
+            taskTitle: 'Segregation',
+          },
+          {
+            id: 1001,
+            taskTitle: 'Recycle Plastics',
+          },
+        ],
       },
-      {
-        user: {
-          id: 104,
-          name: 'han',
-        },
-        post: {
-          todo: [
-            {
-              id: 1000,
-              taskTitle: 'Segregation',
-            },
-            {
-              id: 1001,
-              taskTitle: 'Recycle Plastics',
-            },
-          ],
-        },
+    },
+    {
+      user: {
+        id: 102,
+        name: 'shin',
       },
-    ],
+      post: {
+        todo: [
+          {
+            id: 1000,
+            taskTitle: 'Segregation',
+          },
+          {
+            id: 1001,
+            taskTitle: 'Recycle Plastics',
+          },
+        ],
+      },
+    },
+    {
+      user: {
+        id: 103,
+        name: 'choi',
+      },
+      post: {
+        todo: [
+          {
+            id: 1000,
+            taskTitle: 'Segregation',
+          },
+          {
+            id: 1001,
+            taskTitle: 'Recycle Plastics',
+          },
+        ],
+      },
+    },
+    {
+      user: {
+        id: 104,
+        name: 'han',
+      },
+      post: {
+        todo: [
+          {
+            id: 1000,
+            taskTitle: 'Segregation',
+          },
+          {
+            id: 1001,
+            taskTitle: 'Recycle Plastics',
+          },
+        ],
+      },
+    },
+  ],
 };
-  
+
 export default asia;
