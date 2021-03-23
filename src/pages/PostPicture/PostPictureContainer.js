37154ac9d639f4731be6e9a78b39d7e19ced5143
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-23 23:31:32 +0900

    CSS Migration End!

--- src/pages/PostPicture/PostPictureContainer.js
+++ src/pages/PostPicture/PostPictureContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { useForm } from 'react-hook-form';
 
@@ -11,6 +11,8 @@ import { sendPicture } from '../../redux/actions';
 export default function PostPictureContainer() {
   const dispatch = useDispatch();
   const history = useHistory();
+  const addPhoto = useRef(null);
+  const photoInput = useRef(null);
 
   const { handleSubmit, register } = useForm();
 
@@ -24,24 +26,56 @@ export default function PostPictureContainer() {
     history.push('/community/post/description');
   };
 
+  function handleClickClose() {
+    history.push('/community/post');
+  }
+
+  function onChange() {
+    const file = photoInput.current.firstChild.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        addPhoto.current.style.backgroundImage = `url(${reader.result})`;
+      };
+    }
+  }
+
+  function onClickImage() {
+    photoInput.current.firstChild.click();
+  }
+
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="post-picture">
-          Upload
-        </label>
-        <input
-          type="file"
-          id="post-picture"
-          name="imageFile"
-          accept="image/*"
-          required
-          ref={register()}
+        <div className="head__link-bar">
+          <button type="button" onClick={handleClickClose} className="head__link">
+            BACK
+          </button>
+          <button type="submit">
+            NEXT
+          </button>
+        </div>
+        <div
+          className="community-picture"
+          ref={addPhoto}
+          onClick={onClickImage}
         />
-
-        <button type="submit">
-          NEXT
-        </button>
+        <label className="community-picture__text" htmlFor="post-picture">
+          Click to upload!
+        </label>
+        <div className="community-picture__upload" ref={photoInput}>
+          <input
+            type="file"
+            id="post-picture"
+            name="imageFile"
+            accept="image/*"
+            onChange={onChange}
+            required
+            ref={register()}
+          />
+        </div>
       </form>
     </div>
   );
